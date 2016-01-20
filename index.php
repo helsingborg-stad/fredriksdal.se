354ddab7fecbfe5713a9ce74d604b180c30fc0e7
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-01-20 16:09:59 +0100

    Updated ignore file. Do track use wp-config.php.

--- index.php
+++ index.php
@@ -12,5 +12,17 @@
  */
 define('WP_USE_THEMES', true);
 
+/**
+ * Tells WordPress to load from local wp-content, and not vendor wp.
+ * @var bool
+ */
+define('WP_CONTENT_DIR', dirname(__FILE__) . '/wp-content');
+
+/**
+ * Tells WordPress to load assets from wp-content and not vendor wp. 
+ * @var bool
+ */
+define('WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/wp-content');v
+
 /** Loads the WordPress Environment and Template */
 require(dirname(__FILE__) . '/wp/wp-blog-header.php');
