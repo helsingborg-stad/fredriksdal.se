6b10269db285c461cc7b58ed95567654c49d43d5
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-21 11:54:38 +0100

    Add uploads/cache/blade-cache with the installer script

--- installer.sh
+++ installer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+mkdir -p wp-content/uploads/cache/blade-cache
+
 find . -name package.json -maxdepth 10 -execdir npm update \;
 find . -name composer.json -maxdepth 10 -execdir composer update \;
