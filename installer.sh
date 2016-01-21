cd6802bd2dd5c9cd19e98f0083110ed2b6768666
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-21 13:05:35 +0100

    Composer and installer fixes

--- installer.sh
+++ installer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir -p wp-content/uploads/cache/blade-cache
+php /usr/local/bin/composer update
 
-find . -name package.json -maxdepth 10 -execdir npm update \;
-find . -name composer.json -maxdepth 10 -execdir composer update \;
