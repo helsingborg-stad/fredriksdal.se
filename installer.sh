49fa302e437f83bada27f9842069d2c1780cc416
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-01-21 16:16:17 +0100

    Added installer files (uncomment to run mu-plugin)

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p wp-content/uploads/cache/blade-cache
 php /usr/local/bin/composer update
-
+find . -name package.json -maxdepth 3 -execdir npm install \;
---
+++ wp-content/mu-plugins/installer.php
@@ -0,0 +1,14 @@
+<?php 
+	
+	/* Uncomment this to install wp  */ 
+	/*
+		
+	add_action('init',function() {
+		update_option('siteurl',''http://' . $_SERVER['HTTP_HOST'] . '/wp/' );
+	});
+	
+	add_action('init',function() {
+		update_option('homeurl','http://' . $_SERVER['HTTP_HOST'] );
+	});
+	
+	*/ 
