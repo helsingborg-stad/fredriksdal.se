9a4663ea93326eb9f0c6cdf51d074a4b99b961f3
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-01-28 13:19:19 +0100

    Custom text module template

---
+++ wp-content/themes/dunkerskulturhus/templates/module/modularity-mod-text.php
@@ -0,0 +1,4 @@
+<h4 class="box-title"><?php echo apply_filters('the_title', $module->post_title); ?></h4>
+<div class="box-content gutter">
+    <?php echo apply_filters('the_content', $module->post_content); ?>
+</div>
