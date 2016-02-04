334f56e463d80d1b55e6f3cd46a058220c7b6587
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-04 10:04:16 +0100

    Adds static buy/read more button to event list

--- wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
+++ wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
@@ -1,7 +1,14 @@
 <div class="box box-event" data-equal-item>
-    <img src="{{ get_field('event-image_url') }}" alt="{{ get_the_title() }}" class="box-event-image">
+    <span class="box-image" style="background-image:url('{{ get_field('event-image_url') }}');">
+        <img src="{{ get_field('event-image_url') }}" alt="{{ get_the_title() }}">
+    </span>
     <div class="event-information">
-        <h3 class="event-title">{{ the_title() }}</h3>
-        <span class="event-date-start">{{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}</span>
+        <span class="col">
+            <h3 class="event-title">{{ the_title() }}</h3>
+            <span class="event-date-start">{{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}</span>
+        </span>
+        <span class="col event-action">
+            <a href="#" class="btn btn-circle btn-green"><span>Läs mer</span></a>
+        </span>
     </div>
 </div>
