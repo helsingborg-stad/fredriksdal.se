4496cd305fe83d29f1caca281f28eb8b7e5cf927
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-05 10:48:01 +0100

    Psr class loader instead of manual require

--- wp-content/themes/dunkerskulturhus/functions.php
+++ wp-content/themes/dunkerskulturhus/functions.php
@@ -1,13 +1,17 @@
 <?php
 
+define('DUNKERS_PATH', get_stylesheet_directory() . '/');
+
 //Include vendor files
 if (file_exists(dirname(ABSPATH) . '/vendor/autoload.php')) {
     require_once dirname(ABSPATH) . '/vendor/autoload.php';
 }
 
-//Run theme functions
-require_once get_stylesheet_directory() . '/library/Theme/EventArchive.php';
-require_once get_stylesheet_directory() . '/library/Theme/Enqueue.php';
-require_once get_stylesheet_directory() . '/library/Theme/Navigation.php';
-require_once get_stylesheet_directory() . '/library/App.php';
+require_once DUNKERS_PATH . 'library/Vendor/Psr4ClassLoader.php';
+var_dump(DUNKERS_PATH . 'library/Vendor/Psr4ClassLoader.php');
+$loader = new Dunkers\Vendor\Psr4ClassLoader();
+$loader->addPrefix('Dunkers', DUNKERS_PATH . 'library');
+$loader->addPrefix('Dunkers', DUNKERS_PATH . 'source/php/');
+$loader->register();
+
 new Dunkers\App();
