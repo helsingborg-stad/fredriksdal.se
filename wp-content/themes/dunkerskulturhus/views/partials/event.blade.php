90789238b5ff1a2b22418ec8f0e9e1915a0f4815
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-05 10:36:09 +0100

    Make whole event clickable

--- wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
+++ wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
@@ -1,22 +1,18 @@
-<div class="box box-event" data-equal-item>
+<a href="{{ the_permalink() }}" class="box box-event" data-equal-item>
     <span class="box-image" style="background-image:url('{{ get_field('event-image_url') }}');">
         <img src="{{ get_field('event-image_url') }}" alt="{{ get_the_title() }}">
     </span>
     <div class="event-information">
-        <span class="col">
-            <h3 class="event-title">{{ the_title() }}</h3>
-            <span class="event-date-start">
-                {{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}
-                @if ($post->occations_count > 1)
-                <em style="font-style:italic;">(och {{ $post->occations_count }} andra tillfällen)</em>
-                @endif
-            </span>
-        </span>
-        <span class="col event-action">
-            <a href="{{ the_permalink() }}" class="btn btn-circle btn-green"><span>Läs mer</span></a>
+        <h3 class="event-title">{{ the_title() }}</h3>
+        <span class="event-date-start">
+            {{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}
+            @if ($post->occations_count > 1)
+            <em style="font-style:italic;">(och {{ $post->occations_count }} andra tillfällen)</em>
+            @endif
         </span>
     </div>
     <div class="event-lead">
         {!! the_excerpt() !!}
     </div>
-</div>
+    <span class="event-action btn btn-circle btn-green"><span>Läs mer</span></span>
+</a>
