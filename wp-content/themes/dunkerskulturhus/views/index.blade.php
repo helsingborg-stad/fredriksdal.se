6fcb7060240386ad17475c80e46dd1608efdd8c1
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-09 12:46:38 +0100

    Add content-area-bottom to index template

--- wp-content/themes/dunkerskulturhus/views/index.blade.php
+++ wp-content/themes/dunkerskulturhus/views/index.blade.php
@@ -14,68 +14,7 @@
 
 <div class="container gutter-xl gutter-vertical">
     <div class="grid">
-        <div class="grid-lg-12">
-            <h1 class="text-highlight text-center">Detta händer på Dunkers idag</h1>
-        </div>
-    </div>
-
-    <div class="grid">
-        <div class="grid-md-3 grid-sm-6">
-            <a href="#" class="box box-news">
-                <span class="box-image" style="background-image:url('http://gigguide.se/static/images/performers/661/markus-krunegard-1-original.jpeg');">
-                    <img src="http://gigguide.se/static/images/performers/661/markus-krunegard-1-original.jpeg" alt="Kommunalanställd">
-                </span>
-                <span class="box-content">
-                    <h5 class="link-item link-item-light">Markus Krunegård</h5>
-                    <p>
-                        Akustisk turné<br>
-                        18 mars 2016 kl. 19:00
-                    </p>
-                </span>
-            </a>
-        </div>
-        <div class="grid-md-3 grid-sm-6">
-            <a href="#" class="box box-news">
-                <span class="box-image" style="background-image:url('http://mittkulturkort.se/data/kulturkortet/media/news/image/92/13/46/7/5164/e64c22a79bb566db/thumbs/kk_691_518_DEFAULT_cropfit_NORTH.jpg');">
-                    <img src="http://mittkulturkort.se/data/kulturkortet/media/news/image/92/13/46/7/5164/e64c22a79bb566db/thumbs/kk_691_518_DEFAULT_cropfit_NORTH.jpg" alt="Kommunalanställd">
-                </span>
-                <span class="box-content">
-                    <h5 class="link-item link-item-light">Arty after work</h5>
-                    <p>
-                        Häng ut efter jobb<br>
-                        Fredagar kl. 16:00
-                    </p>
-                </span>
-            </a>
-        </div>
-        <div class="grid-md-3 grid-sm-6">
-            <a href="#" class="box box-news">
-                <span class="box-image" style="background-image:url('http://vansterparlan.v-blog.se/files/2014/07/Athena.jpg');">
-                    <img src="http://vansterparlan.v-blog.se/files/2014/07/Athena.jpg" alt="Kommunalanställd">
-                </span>
-                <span class="box-content">
-                    <h5 class="link-item link-item-light">Samtal på Dunkers</h5>
-                    <p>
-                        Athena Farrokhzad<br>
-                        16 mars 2016 14:00
-                    </p>
-                </span>
-            </a>
-        </div>
-        <div class="grid-md-3 grid-sm-6">
-            <a href="#" class="box box-news">
-                <span class="box-image" style="background-image:url('http://vansterparlan.v-blog.se/files/2014/07/Athena.jpg');">
-                    <img src="http://vansterparlan.v-blog.se/files/2014/07/Athena.jpg" alt="Kommunalanställd">
-                </span>
-                <span class="box-content">
-                    <h5 class="link-item link-item-light">Samtal på Dunkers</h5>
-                    <p>
-                        Athena Farrokhzad<br>
-                        16 mars 2016 14:00
-                    </p>
-                </span>
-            </a>
-        </div>
+        {{ dynamic_sidebar('content-area-bottom') }}
     </div>
 
     <div class="grid">
