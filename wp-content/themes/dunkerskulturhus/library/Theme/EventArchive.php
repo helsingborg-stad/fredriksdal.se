d31345745b923bd76c05ad451e15770ad6b0f78c
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-12 10:28:00 +0100

    Fixes event query issue

--- wp-content/themes/dunkerskulturhus/library/Theme/EventArchive.php
+++ wp-content/themes/dunkerskulturhus/library/Theme/EventArchive.php
@@ -32,7 +32,7 @@ class EventArchive
     public function sqlGroupBy($groupBy)
     {
         global $wpdb;
-        $groupBy = $wpdb->prefix . '.post_title';
+        $groupBy = $wpdb->prefix . $wpdb->posts . '.post_title';
         return $groupBy;
     }
 
