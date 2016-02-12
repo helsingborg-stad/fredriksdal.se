e691e7b3f834877b43423aab7ad34fe101ac264e
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-12 13:14:46 +0100

    Content-area display fixes

--- wp-content/themes/dunkerskulturhus/views/page.blade.php
+++ wp-content/themes/dunkerskulturhus/views/page.blade.php
@@ -5,7 +5,7 @@
 <div class="container no-margin-top">
     @include('partials.breadcrumbs')
 
-    <div class="grid no-margin-top">
+    <main class="grid no-margin-top">
         @include('partials.sidebar-left')
 
         <div class="{{ is_active_sidebar('right-sidebar') ? 'grid-md-8 grid-lg-6' : 'grid-md-8' }}">
@@ -15,13 +15,17 @@
                 @include('partials.article')
             @endwhile
 
-            {!! dynamic_sidebar('content-area') !!}
+            @if (is_active_sidebar('content-area'))
+            <div class="grid gutter gutter-lg gutter-top">
+                {!! dynamic_sidebar('content-area') !!}
+            </div>
+            @endif
         </div>
 
         @if (is_active_sidebar('right-sidebar'))
             @include('partials.sidebar-right')
         @endif
-    </div>
+    </main>
 </div>
 
 @stop
