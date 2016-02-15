caa7bee93f043b0bb0cab6c78b2dd48911ae4fc5
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-02-15 11:32:19 +0100

    Do not show empty image

--- wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
+++ wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
@@ -1,10 +1,14 @@
 <a href="{{ the_permalink() }}" class="box box-event">
+    @if (is_string(get_field('event-image_url')))
     <span class="box-image" style="background-image:url('{{ get_field('event-image_url') }}');">
         <img src="{{ get_field('event-image_url') }}" alt="{{ get_the_title() }}">
     </span>
+    @endif
+
     <div class="event-information">
         <time class="event-date-start">
             {{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}
+
             @if ($post->occations_count > 1)
             <em style="font-style:italic;">(och {{ $post->occations_count }} andra tillfällen)</em>
             @endif
