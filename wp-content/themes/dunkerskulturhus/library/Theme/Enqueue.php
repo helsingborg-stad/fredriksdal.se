0cfa924b824434c1b94af23be27c4714440e18eb
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-02-24 15:29:16 +0100

    Version handling on local files.

--- wp-content/themes/dunkerskulturhus/library/Theme/Enqueue.php
+++ wp-content/themes/dunkerskulturhus/library/Theme/Enqueue.php
@@ -10,7 +10,6 @@ class Enqueue
         add_action('wp_enqueue_scripts', array($this, 'style'));
         add_action('wp_enqueue_scripts', array($this, 'script'));
         add_action('wp_enqueue_scripts', array($this, 'font'));
-
     }
 
     /**
@@ -21,7 +20,7 @@ class Enqueue
     {
         wp_register_style('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/css/hbg-prime.min.css', '', '1.0.0');
         wp_enqueue_style('hbg-prime');
-        wp_enqueue_style('dunkers-css', get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '', '1.0.0');
+        wp_enqueue_style('dunkers-css', get_stylesheet_directory_uri(). '/assets/dist/css/app.min.css', '', filemtime(get_stylesheet_directory() . '/assets/dist/css/app.min.css'));
     }
 
     /**
@@ -30,7 +29,7 @@ class Enqueue
      */
     public function script()
     {
-        wp_enqueue_script('dunkers-js', get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '', '1.0.0', true);
+        wp_enqueue_script('dunkers-js', get_stylesheet_directory_uri(). '/assets/dist/js/app.min.js', '', filemtime(get_stylesheet_directory() . '/assets/dist/js/app.min.js'), true);
         wp_register_script('hbg-prime', 'http://helsingborg-stad.github.io/styleguide-web-cdn/styleguide.dev/dist/js/hbg-prime.min.js', '', '1.0.0', true);
         wp_enqueue_script('hbg-prime');
     }
@@ -41,10 +40,7 @@ class Enqueue
      */
     public function font()
     {
-        wp_enqueue_style('karbon-light', get_stylesheet_directory_uri(). '/assets/font/karbon/Karbon-Light.css', '', '1.0.0');
-        wp_enqueue_style('karbon-semibold', get_stylesheet_directory_uri(). '/assets/font/karbon/Karbon-Semibold.css', '', '1.0.0');
+        wp_enqueue_style('karbon-light', get_stylesheet_directory_uri(). '/assets/font/karbon/Karbon-Light.css', '', filemtime(get_stylesheet_directory() . '/assets/font/karbon/Karbon-Light.css'));
+        wp_enqueue_style('karbon-semibold', get_stylesheet_directory_uri(). '/assets/font/karbon/Karbon-Semibold.css', '', filemtime(get_stylesheet_directory() . '/assets/font/karbon/Karbon-Semibold.css'));
     }
-
 }
-
-
