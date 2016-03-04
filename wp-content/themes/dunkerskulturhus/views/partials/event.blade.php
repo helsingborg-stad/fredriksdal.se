c777a41d35b5a65e859bf49f1993aa3e8a0f6046
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-03-04 11:11:42 +0100

    Corrected html time element

--- wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
+++ wp-content/themes/dunkerskulturhus/views/partials/event.blade.php
@@ -15,13 +15,12 @@
     @endif
 
     <div class="event-information">
-        <time class="event-date-start">
-            {{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}
-
+        <span class="event-date-start">
+            <time datetime="{{ date('Y-m-d H:i', strtotime(get_field('event-date-start'))) }}">{{ date('Y-m-d \k\l\. H:i', strtotime(get_field('event-date-start'))) }}</time>
             @if ($post->occations_count > 1)
             <em style="font-style:italic;">(och {{ $post->occations_count }} andra tillfällen)</em>
             @endif
-        </time>
+        </span>
         <h3 class="event-title">{{ the_title() }}</h3>
     </div>
     <div class="event-lead">
