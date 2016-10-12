b1331010d0d50acfefc291371d2c66f63a8797a9
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-10-12 09:37:34 +0200

    Header updates.

--- wp-content/themes/fredriksdal/views/partials/header.blade.php
+++ wp-content/themes/fredriksdal/views/partials/header.blade.php
@@ -16,6 +16,25 @@
 
                     {!! municipio_get_logotype(get_field('header_logotype', 'option'), get_field('logotype_tooltip', 'option')) !!}
 
+                    {!!
+                        wp_nav_menu(array(
+                            'theme_location' => 'header-tabs-menu',
+                            'container' => 'nav',
+                            'container_class' => 'hidden-print hidden-xs hidden-sm hidden-md',
+                            'container_id' => 'header-tabs',
+                            'menu_class' => 'small navbar',
+                            'menu_id' => 'help-menu-top-bar',
+                            'echo' => 'echo',
+                            'before' => '',
+                            'after' => '',
+                            'link_before' => '',
+                            'link_after' => '',
+                            'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+                            'depth' => 1,
+                            'fallback_cb' => '__return_false'
+                        ));
+                    !!}
+
                     {!! $navigation['mainMenu'] !!}
                     <a href="#mobile-menu" data-target="#mobile-menu" class="{!! apply_filters('Municipio/mobile_menu_breakpoint','hidden-md hidden-lg'); !!} menu-trigger"><span class="menu-icon"></span></a>
                 </div>
