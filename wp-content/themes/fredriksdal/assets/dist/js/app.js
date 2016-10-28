0f4af64d0f67f9d0517914e2b244a8e8752d3179
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-10-28 16:42:17 +0200

    More modules scrolling sideways

--- wp-content/themes/fredriksdal/assets/dist/js/app.js
+++ wp-content/themes/fredriksdal/assets/dist/js/app.js
@@ -104,19 +104,27 @@ Fredriksdal.VerticalScroll.VerticalScroll = (function ($) {
     var VertialScrollWrapper = '.modularity-onepage-section.section-horizontal-scroll';
 
     var VerticalScrollTargets = [
-        '.grid .widget-slider'
+        '.widget-slider.grid .modularity-mod-posts .grid',
+        '.widget-slider.grid .modularity-mod-index .grid',
+        '.widget-slider.grid .modularity-mod-social ul.social-feed-instagram'
     ];
 
     var VerticalScrollSettigs = {
         cellAlign: 'left',
-        contain: true
+        contain: true,
+        percentPosition: true
     };
 
     function VerticalScroll() {
+        this.htmlWrapper();
         VerticalScrollTargets.forEach(function(element) {
             jQuery(VertialScrollWrapper + ' ' + element).flickity(VerticalScrollSettigs);
         }.bind(this));
     }
+
+    VerticalScroll.prototype.htmlWrapper = function () {
+       jQuery('html').addClass('no-flexbox');
+    };
 
     new VerticalScroll();
 
--- wp-content/themes/fredriksdal/library/Theme/OnePage.php
+++ wp-content/themes/fredriksdal/library/Theme/OnePage.php
@@ -8,12 +8,10 @@ class OnePage
     {
         add_filter('ModularityOnePage/section_class', array($this, 'addCustomClass'), 10, 3);
 
-        add_action('ModularityOnePage/after_section', array($this, 'addSectionContentAfter'));
-        add_action('ModularityOnePage/before_section', array($this, 'addSectionContentBefore'));
-
         add_action('ModularityOnePage\after_modules', array($this, 'addWidgetContentAfter'));
         add_action('ModularityOnePage\before_modules', array($this, 'addWidgetContentBefore'));
 
+        add_filter('get_post_metadata', array($this, 'removeAllModuleTitles'), 100, 4);
     }
 
     public function addCustomClass($input, $postId)
@@ -24,27 +22,23 @@ class OnePage
         return array_filter(array_merge($input, (array) $post_data));
     }
 
-    public function addSectionContentBefore($input)
-    {
-        $html = array(
-            '<button class="prevPage">' .__('Previous'). '</button>',
-            '<button class="nextPage">' .__('Next'). '</button>'
-        );
-        return $input.implode($html, '');
-    }
-
-    public function addSectionContentAfter($input)
-    {
-        return '<ul class="pages"></ul>'.$input;
-    }
-
     public function addWidgetContentBefore($input)
     {
-        return $input.'<div class="widget-slider">';
+        return '<div class="widget-slider grid">';
     }
 
     public function addWidgetContentAfter($input)
     {
-        return '</div>'.$input;
+        return '</div>';
+    }
+
+    public function removeAllModuleTitles($metadata, $object_id, $meta_key, $single)
+    {
+        if (isset($meta_key) && $meta_key == 'modularity-module-hide-title') {
+            if (!in_array(get_post_type($object_id), array('mod-text'))) {
+                return true;
+            }
+        }
+        return $metadata;
     }
 }
