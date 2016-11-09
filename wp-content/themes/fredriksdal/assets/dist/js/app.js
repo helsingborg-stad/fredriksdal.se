f5f1c111c0a84e0f1b49a7761470bc081877ffc1
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-09 12:22:14 +0100

    Overlay iframes mathing pattern with clickable layer

--- wp-content/themes/fredriksdal/assets/dist/js/app.js
+++ wp-content/themes/fredriksdal/assets/dist/js/app.js
@@ -298,6 +298,35 @@ Fredriksdal.HorizontalScroll.HorizontalScroll = (function ($) {
 })(jQuery);
 
 Fredriksdal = Fredriksdal || {};
+Fredriksdal.IframeTrigger = Fredriksdal.IframeTrigger || {};
+
+Fredriksdal.IframeTrigger.IframeTrigger = (function ($) {
+
+    var DisabledElementClass = 'disabled-iframe';
+
+    var TargetIframes = [
+        "iframe[src*='maps']",
+    ];
+
+    function IframeTrigger() {
+        TargetIframes   .forEach(function(element) {
+            jQuery(element).parent().addClass(DisabledElementClass);
+            this.bindEnableClick(jQuery(element).parent());
+        }.bind(this));
+    }
+
+    IframeTrigger.prototype.bindEnableClick = function (target) {
+        jQuery(target).on('click',function(event){
+            event.preventDefault();
+            jQuery(target).removeClass(DisabledElementClass);
+        }.bind(this));
+    };
+
+    return new IframeTrigger();
+
+})(jQuery);
+
+Fredriksdal = Fredriksdal || {};
 Fredriksdal.MobileMenuAnchorClose = Fredriksdal.MobileMenuAnchorClose || {};
 
 Fredriksdal.MobileMenuAnchorClose.MobileMenuAnchorClose = (function ($) {
