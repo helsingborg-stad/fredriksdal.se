367adb6180fa95b392485ad128513466d51fd613
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-11 13:03:08 +0100

    If anchor dosen’t exists, fallback to front-page

--- wp-content/themes/fredriksdal/assets/source/js/anchorscroll.js
+++ wp-content/themes/fredriksdal/assets/source/js/anchorscroll.js
@@ -23,6 +23,9 @@ Fredriksdal.AnchorScroll.AnchorScroll = (function ($) {
                 if(this.isAnchorLink(jQuery(item).attr('href')) && this.anchorLinkExists(jQuery(item).attr('href'))) {
                     this.bindAnchorScroll(item,jQuery(item).attr('href'));
                 }
+                if(this.isAnchorLink(jQuery(item).attr('href')) && !this.anchorLinkExists(jQuery(item).attr('href'))) {
+                    jQuery(item).attr('href', location.protocol + "//" + window.location.hostname  + "/" + jQuery(item).attr('href'));
+                }
             }.bind(this));
         }.bind(this));
     }
