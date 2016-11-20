1a65dbc0bd2acc21b385baaa0bf40eb18fdf37d8
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-20 15:09:05 +0100

    Check is is visible by plugin.

--- wp-content/themes/fredriksdal/assets/source/js/asynccontent.js
+++ wp-content/themes/fredriksdal/assets/source/js/asynccontent.js
@@ -144,20 +144,8 @@ Fredriksdal.AsyncContentLoader.AsyncContentLoader = (function ($) {
     };
 
     /* Is in viewport */
-    AsyncContentLoader.prototype.isInViewport = function (el) {
-
-        if (typeof jQuery === "function" && el instanceof jQuery) {
-            el = el[0];
-        }
-
-        var rect = el.getBoundingClientRect();
-
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
+    AsyncContentLoader.prototype.isInViewport = function (element) {
+        return jQuery(element).visible(true);
     }
 
     /* Update hash */
