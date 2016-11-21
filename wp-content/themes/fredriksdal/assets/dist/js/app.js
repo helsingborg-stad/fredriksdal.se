91f353e47879337eb9eb11ccaf411b60a40d7d82
Kristoffer Svanmark <kristoffer.svanmark@lexiconitkonsult.se>
2016-11-21 14:51:37 +0100

    Park map

--- wp-content/themes/fredriksdal/assets/dist/js/app.js
+++ wp-content/themes/fredriksdal/assets/dist/js/app.js
@@ -544,6 +544,38 @@ Fredriksdal.OffGrid.OffGrid = (function ($) {
 
 })(jQuery);
 
+function initMap() {
+    var srcImage = 'http://fredriksdal.dev/Fredriksdal_karta_V3.svg';
+
+    // Initialize the map
+    var map = new google.maps.Map(document.getElementById('park-map'), {
+        zoom: 17,
+        center: {lat: 56.0574959, lng: 12.7108654},
+        mapTypeId: 'roadmap'
+    });
+
+    // Add park map
+    var imageBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(56.05357504224284, 12.707661319732551),
+        new google.maps.LatLng(56.06076274162973, 12.71944792938234)
+    );
+
+    var mapOverlay = new google.maps.GroundOverlay(
+        srcImage,
+        imageBounds
+    );
+
+    console.log(mapOverlay);
+
+    mapOverlay.setMap(map);
+}
+
+$(document).ready(function () {
+    if (document.getElementById('park-map')) {
+        google.maps.event.addDomListener(window, 'load', initMap);
+    }
+});
+
 Fredriksdal = Fredriksdal || {};
 Fredriksdal.ScrollHighlight = Fredriksdal.ScrollHighlight || {};
 
