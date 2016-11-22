4b60efc1bb877271a81cfa96e538ff7bdbb0ae52
Sebastian Thulin <sebastian.thulin@gmail.com>
2016-11-22 11:13:55 +0100

    Removed ;

--- wp-content/themes/fredriksdal/views/partials/search/top-search.blade.php
+++ wp-content/themes/fredriksdal/views/partials/search/top-search.blade.php
@@ -5,7 +5,7 @@
 <div class="search-top {!! apply_filters('Municipio/desktop_menu_breakpoint','hidden-sm'); !!}" id="search">
     <button type="button" class="btn btn-plain pricon pricon-close toggle-search-top"><?php _e('Close', 'fredriksdal'); ?></button>
 
-    <form method="get" action="{{ home_url(); }}">
+    <form method="get" action="{{ home_url() }}">
         <label for="searchkeyword-{{ $searchFormNode }}">{{ get_field('search_placeholder_text', 'option') ? get_field('search_placeholder_text', 'option') : 'What are you looking for?' }}</label>
         <div class="input-group input-group-lg">
             <input id="searchkeyword-{{ $searchFormNode }}" autocomplete="off" class="form-control form-control-lg" type="search" name="s" value="<?php echo (!empty(get_search_query())) ? get_search_query() : ''; ?>">
