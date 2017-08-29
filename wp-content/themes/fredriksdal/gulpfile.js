7697d04e905291b36659e2b9744c277a5a476f35
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2017-08-29 14:37:02 +0200

    IPAD Style fix: Horizontal Slider Arrows
    + Fix slider arrows style on Ipad
    + Added browserSync to Gulp for easier development
    
    Ticket: https://intranat.helsingborg.se/arende/pilarna-pa-den-horisontella-slidern-blir-beskurna/

--- wp-content/themes/fredriksdal/gulpfile.js
+++ wp-content/themes/fredriksdal/gulpfile.js
@@ -8,6 +8,8 @@ var uglify 			= require('gulp-uglify');
 var cssnano 		= require('gulp-cssnano');
 var rename 			= require('gulp-rename');
 var autoprefixer 	= require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
 
 // Compile Our Sass
 gulp.task('sass-dist', function() {
@@ -19,7 +21,8 @@ gulp.task('sass-dist', function() {
                 mergeLonghand: false,
                 zindex: false
             }))
-            .pipe(gulp.dest('assets/dist/css'));
+            .pipe(gulp.dest('assets/dist/css'))
+            .pipe(browserSync.stream());
 });
 
 gulp.task('sass-dev', function() {
@@ -27,7 +30,8 @@ gulp.task('sass-dev', function() {
             .pipe(sass())
             .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
             .pipe(rename({suffix: '.dev'}))
-            .pipe(gulp.dest('assets/dist/css'));
+            .pipe(gulp.dest('assets/dist/css'))
+            .pipe(browserSync.stream());
 });
 
 // Concatenate & Minify JS
@@ -49,3 +53,17 @@ gulp.task('watch', function() {
 // Default Task
 gulp.task('default', ['sass-dist', 'sass-dev', 'scripts-dist', 'watch']);
 
+//BrowserSync
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        proxy: "https://fredriksdal.dev",
+        https: true
+    });
+});
+
+//Watch with BrowserSync
+gulp.task('watch-bs', ['browser-sync'], function () {
+    gulp.watch('assets/source/js/**/*.js', ['scripts-dist', browserSync.reload]);
+    gulp.watch('assets/source/sass/**/*.scss', ['sass-dist', 'sass-dev']);
+
+});
