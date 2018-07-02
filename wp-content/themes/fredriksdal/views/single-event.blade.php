762c25105cf82403346aa08cfcdb0dbc84762d51
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-07-02 15:52:28 +0200

    Make single view compatible with new event API

--- wp-content/themes/fredriksdal/views/single-event.blade.php
+++ wp-content/themes/fredriksdal/views/single-event.blade.php
@@ -2,20 +2,19 @@
 
 @section('content')
 {!! the_post() !!}
-
 <section class="background-white gutter gutter-vertical gutter-xl">
     <div class="container">
         <div class="grid gutter gutter-vertical gutter-xl">
             <div class="grid-md-9 grid-lg-9 grid-sm-12">
-                <?php global $post; ?>
+
                 <article class="clearfix">
                     <header>
                     <h1 {!! $image ? 'class="hidden-md hidden-lg"' : '' !!}>
                         <div class="tc">
                             <time class="date-box" datetime="{{ get_post_meta(get_the_id(), 'event-date-start', true) }}">
                                 <div>
-                                    <span class="day">{{ mysql2date('j', get_post_meta(get_the_id(), 'event-date-start', true)) }}</span>
-                                    <span class="month">{{ mysql2date('M', get_post_meta(get_the_id(), 'event-date-start', true)) }}</span>
+                                    <span class="day">{{ $occasion['date_parts']['date'] ?? '' }}</span>
+                                    <span class="month">{{ $occasion['date_parts']['month'] ?? '' }}</span>
                                 </div>
                             </time>
                         </div>
@@ -61,10 +60,10 @@
                         <div class="date">
                             <label><?php _e('Date', 'fredriksdal'); ?></label>
 
-                            @if (strtotime(get_post_meta(get_the_id(), 'event-date-end', true)) >= time())
-                                <span class="value">{{ mysql2date('Y-m-d', get_post_meta(get_the_id(), 'event-date-start', true)) }}</span>
-                            @elseif(count($passedOccations) > 0)
-                                <span class="value" style="text-decoration: line-through;">{{ mysql2date('Y-m-d', $passedOccations[0]) }}</span>
+                            @if (strtotime($occasion['start_date']) >= time())
+                                <span class="value">{{explode(' ', $occasion['start_date'])[0]}}</span>
+                            @elseif(count($occations) > 0)
+                                <span class="value" style="text-decoration: line-through;">{{explode(' ', $occasion['start_date'])[0]}}</span>
                             @else
                                 <span class="value">Passerat</span>
                             @endif
@@ -76,15 +75,15 @@
 
                         </div>
 
-                        @if (!empty(get_post_meta(get_the_id(), 'event-date-start', true)))
+                        @if (is_array(explode(' ', $occasion['start_date'])) && count(explode(' ', $occasion['start_date'])) > 1 && is_array(explode(' ', $occasion['end_date'])) && count(explode(' ', $occasion['end_date'])) > 1)
                         <div class="time clearfix">
                             <div class="time-start">
                                 <label><?php _e('From', 'fredriksdal'); ?></label>
-                                <div class="value">{{ mysql2date('H:i', get_post_meta(get_the_id(), 'event-date-start', true)) }}</div>
+                                <div class="value">{{ mysql2date('H:i', $occasion['start_date']) }}</div>
                             </div>
                             <div class="time-end">
                                 <label><?php _e('To', 'fredriksdal'); ?></label>
-                                <div class="value">{{ mysql2date('H:i', get_post_meta(get_the_id(), 'event-date-end', true)) }}</div>
+                                <div class="value">{{ mysql2date('H:i', $occasion['end_date']) }}</div>
                             </div>
                         </div>
                         @endif
@@ -96,23 +95,21 @@
                     @endif
                     </div>
 
-                    <?php //var_dump($passedOccations); ?>
-
-                    @if (isset($occations) && isset($passedOccations) && (count($occations) > 0 || count($passedOccations) > 0 ))
+                    @if (isset($occations) && (count($occations) > 0 ))
                     <div class="box-ticket-occasions">
+
                         <p>
                             <strong>{{ the_title() }}</strong> kan också besökas på:
                         </p>
                         <p>
                             <ul>
-                                @foreach ($passedOccations as $occation)
+                                @foreach ($occations as $eventOccation)
                                     <li>
-                                        <span style="text-decoration: line-through;" class="link-item" >{{ \Municipio\Helper\Dt::dateWithTime(strtotime(get_field('event-date-start', $occation->ID))) }}</span>
-                                    </li>
-                                @endforeach
-                                @foreach ($occations as $occation)
-                                    <li>
-                                        <a class="link-item" href="{{ get_permalink($occation->ID) }}">{{ \Municipio\Helper\Dt::dateWithTime(strtotime(get_field('event-date-start', $occation->ID))) }}</a>
+                                        @if (strtotime($eventOccation->start_date) <= time())
+                                        <a class="link-item" style="text-decoration: line-through;" href="{{ esc_url(add_query_arg('date', preg_replace('/\D/', '', $eventOccation->start_date), the_permalink())) }}">{{ \Municipio\Helper\Dt::dateWithTime(strtotime($eventOccation->start_date)) }}</a>
+                                        @else
+                                        <a class="link-item" href="{{ esc_url(add_query_arg('date', preg_replace('/\D/', '', $eventOccation->start_date), the_permalink())) }}">{{ \Municipio\Helper\Dt::dateWithTime(strtotime($eventOccation->start_date)) }}</a>
+                                        @endif
                                     </li>
                                 @endforeach
                             </ul>
