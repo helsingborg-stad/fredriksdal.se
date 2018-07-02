7f0caf6b0c0fe2f2ff1cb1155d9a276dd18841b1
Nikolas Ramstedt <nikolas.ramstedt@gmail.com>
2018-07-02 15:54:01 +0200

    Use Municipio archive-filter instead

--- wp-content/themes/fredriksdal/views/archive-event.blade.php
+++ wp-content/themes/fredriksdal/views/archive-event.blade.php
@@ -5,64 +5,9 @@
 
 <section class="background-lgren events-wrapper">
     <nav class="event-dates text-center">
-        <div class="container">
-            <div class="grid">
-                <div class="grid-sm-12">
-                    <ul class="nav-horizontal nav-justify">
-                        @foreach($quaters as $quater)
-                        <li>
-                            <a href="{{ $quater->url }}" class="{{ $quater->is_active ? 'active' : '' }}">
-                                <span class="year">{{ $quater->year }}</span>
-                                <span class="months"><span class="start-month">{{ $quater->start_month }}</span> - <span class="end-month">{{ $quater->end_month }}</span></span>
-                            </a>
-                        </li>
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-        </div>
     </nav>
 
-    <section class="creamy creamy-border-bottom gutter-lg gutter-vertical sidebar-content-area archive-filters">
-        <form method="get" action="./" class="container" id="archive-filter">
 
-            <div class="max-width-container">
-                <div class="grid">
-
-                    <input type="text" name="from" value="{{ sanitize_text_field($_GET['from']) }}" hidden />
-                    <input type="text" name="to" value="{{ sanitize_text_field($_GET['to']) }}" hidden />
-                    <input type="text" name="filter" value="{{ sanitize_text_field($_GET['filter']) }}" hidden />
-
-                    <div class="grid-xs-12">
-                        <div class="input-group">
-                            <input type="text" name="search" id="filter-keyword" class="form-control" value="{{ sanitize_text_field($_GET['search']) }}" placeholder="<?php _e("Ange ett sökord"); ?>">
-                            <span class="input-group-addon-btn">
-                                <input type="submit" value="<?php _e("Sök"); ?>" class="btn btn-primary btn-block">
-                            </span>
-                        </div>
-                    </div>
-
-                    @if (is_array($links) && !empty($links))
-                        <div class="grid-xs-12">
-                            <ul id="event-categories" class="nav nav-horizontal">
-                                @foreach($links as $link)
-                                    <li class="{{ $link->classes }}">
-                                        @if(preg_match("/current/i", $link->classes))
-                                            <a class="close-me" href="{{ $baseLink }}"><i class="pricon pricon-close"></i></a>
-                                        @endif
-                                        <a href="{!! $link->link !!}">{{ $link->title }}</a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-
-                </div>
-            </div>
-
-        </form>
-
-    </section>
 
     @if (is_active_sidebar('content-area-top'))
     <div class="grid">
@@ -71,7 +16,7 @@
     @endif
 
 </section>
-
+@include('partials.archive-filters')
     @if (have_posts())
         <?php global $post; ?>
         <?php $i = 0; ?>
