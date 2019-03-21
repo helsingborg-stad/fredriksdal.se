7bf641784c8987fa559281f8f47664fc367dbc87
Jonatan Hanson <jonatan.hanson@hiq.se>
2019-03-21 10:36:38 +0100

    Extend base controller

--- wp-content/themes/fredriksdal/library/Controller/SingleEvent.php
+++ wp-content/themes/fredriksdal/library/Controller/SingleEvent.php
@@ -2,7 +2,7 @@
 
 namespace Fredriksdal\Controller;
 
-class SingleEvent extends \Municipio\Controller\SingleEvent
+class SingleEvent extends \Municipio\Controller\BaseController
 {
     public function init()
     {
